df7f9c51f8ac7bcf8dc6c911f09d5efff9b6e2b0
rdev014 <rdev01431@gmail.com>
2025-01-07 13:30:36 +0530

    function to find user

--- introduction/problem.js
+++ introduction/problem.js
@@ -1,4 +1,26 @@
 // Users databse
-const users = ["john","rahul","kuldeep","raj","andrew","david"]
+const users = ["john", "rahul", "kuldeep", "raj", "andrew", "david"]
 
-// To Do Create a Alogorith to find perticular user in short create a function in js to find one user
+// To Do Create a Alogorithm to find perticular user in short create a function in js to find one user
+
+// Solution
+
+// Found : rahul
+
+const findUser = (users, name) => {
+    let found = false;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i] === name) {
+            console.log(`Found : ${name}`);
+            found = true;
+            break;
+           
+        }
+    }
+    if (!found) {
+        console.log(`Not Found: ${name}`);
+    }
+    
+
+}
+findUser(users, "rahul")
