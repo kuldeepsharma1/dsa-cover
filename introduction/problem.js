1ee36dfc5958be159560a27dcef9e93adf836451
kuldeepsharma1 <hytek112@gmail.com>
2025-01-07 12:49:07 +0530

    Add initial user database and outline for user search function

---
+++ introduction/problem.js
@@ -0,0 +1,4 @@
+// Users databse
+const users = ["john","rahul","kuldeep","raj","andrew","david"]
+
+// To Do Create a Alogorith to find perticular user in short create a function in js to find one user
